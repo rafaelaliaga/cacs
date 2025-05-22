1a433b508c280e85addf56f22885e393aa8269a1
rafaelaliaga <rafsaliaga@gmail.com>
2025-05-22 00:56:39 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// ① cria __dirname em ambiente ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/CACS/",      //  ←  coloque exatamente o nome do repositório
+  base: "/cacs/",                       // domínio GitHub Pages
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")   // ② alias indispensável
+    }
+  }
 });
