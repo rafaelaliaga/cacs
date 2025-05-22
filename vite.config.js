7cef1c3f2b353571c1ff8e3a1c00c1d4539344fb
rafaelaliaga <rafsaliaga@gmail.com>
2025-05-22 00:37:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// cria __dirname em ambiente ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/NOME_REPO/",      //  ←  coloque exatamente o nome do repositório
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),   // agora funciona
-    },
-  },
-    base: "/CACS/", 
 });
