0fcc040bba0c871111f57ade5847df0a4c5a4595
rafaelaliaga <rafsaliaga@gmail.com>
2025-05-22 00:37:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/NOME_REPO/",      //  ←  coloque exatamente o nome do repositório
+  base: "/CACS/",      //  ←  coloque exatamente o nome do repositório
   plugins: [react()],
 });
